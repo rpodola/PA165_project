1e0b03502f306120c7718a0c1cf6d178d833a5f6
Radim Podola <xpodola@nymfe83.fi.muni.cz>
2017-10-23 18:26:22 +0200

    Create base Java project.

---
+++ CaloriesRecordingSystem/src/main/java/com/muni/fi/pa165project/App.java
@@ -0,0 +1,13 @@
+package com.muni.fi.pa165project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
