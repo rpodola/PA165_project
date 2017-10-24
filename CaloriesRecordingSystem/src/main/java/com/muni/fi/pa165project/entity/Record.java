102e151e08a1e035fec432eefbce95df3f6ffb42
Peter Krasnan <peter.krasnan@gmail.com>
2017-10-24 18:26:24 +0200

    Add Derby dependency, Hinernate configuration.

--- CaloriesRecordingSystem/src/main/java/com/muni/fi/pa165project/entity/Record.java
+++ CaloriesRecordingSystem/src/main/java/com/muni/fi/pa165project/entity/Record.java
@@ -5,10 +5,80 @@
  */
 package com.muni.fi.pa165project.entity;
 
+import com.muni.fi.pa165project.enums.Difficulty;
+import java.time.LocalDateTime;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  *
- * @author Datart-asus
+ * @author Peter Krasnan
  */
+@Entity
 public class Record {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime time;
+
+    private int burnedCalories;
+
+    private Difficulty difficulty;
+
+    private int distance;
+
+    private int duration;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalDateTime time) {
+        this.time = time;
+    }
+
+    public int getBurnedCalories() {
+        return burnedCalories;
+    }
+
+    public void setBurnedCalories(int burnedCalories) {
+        this.burnedCalories = burnedCalories;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
 }
