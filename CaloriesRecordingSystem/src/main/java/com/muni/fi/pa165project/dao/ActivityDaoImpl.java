72e5e15f48ff54fe182bd71e9b8dbdffb922d336
Radim Podola <rpodola@gmail.com>
2017-10-25 18:13:50 +0200

    Add Activity DAO implementation.

---
+++ CaloriesRecordingSystem/src/main/java/com/muni/fi/pa165project/dao/ActivityDaoImpl.java
@@ -0,0 +1,56 @@
+package com.muni.fi.pa165project.dao;
+
+import com.muni.fi.pa165project.entity.Activity;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Radim Podola
+ */
+@Repository
+public class ActivityDaoImpl implements ActivityDao{
+
+    @PersistenceContext
+    private EntityManager em;
+    
+    @Override
+    public Activity findById(long id) {
+        return this.em.find(Activity.class, id);
+    }
+
+    @Override
+    public void create(Activity activity) {
+        this.em.persist(activity);
+    }
+
+    @Override
+    public void update(Activity activity) {
+        this.em.merge(activity);
+    }
+
+    @Override
+    public void delete(Activity activity) {
+        this.em.remove(activity);
+    }
+
+    @Override
+    public List<Activity> findAll() {
+        return this.em.createQuery("SELECT a from Activity a",
+                Activity.class).getResultList();
+    }
+
+    @Override
+    public List<Activity> findByName(String text) {
+        TypedQuery<Activity> query = em.createQuery(
+                "SELECT a FROM Activity a WHERE a.name LIKE :text",
+                Activity.class);
+        query.setParameter("text", "%"+text+"%");
+        
+        return query.getResultList();
+    }
+    
+}
