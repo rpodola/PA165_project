f6f540dbb45e53100499527efe0c6920bf6c8282
Peter Krasnan <peter.krasnan@gmail.com>
2017-10-25 21:44:13 +0200

    Fixed Record and BurnedCaloriesDao + added ignore files for intellij in .gitignore

--- CaloriesRecordingSystem/src/main/java/com/muni/fi/pa165project/dao/BurnedCaloriesDao.java
+++ CaloriesRecordingSystem/src/main/java/com/muni/fi/pa165project/dao/BurnedCaloriesDao.java
@@ -1,18 +1,47 @@
 package com.muni.fi.pa165project.dao;
 
 import com.muni.fi.pa165project.entity.BurnedCalories;
+
 import java.util.List;
 
 /**
- *
  * @author Peter Krasnan
  */
 public interface BurnedCaloriesDao {
+
+    /**
+     * Finds BurnedCalories by id
+     *
+     * @param id
+     * @return found Burned Calories
+     */
     BurnedCalories findById(long id);
+
+    /**
+     * Returns all BurnedCalories
+     *
+     * @return list of all BurnedCalories
+     */
     List<BurnedCalories> findAll();
-    List<BurnedCalories> findByName();
-    
+
+    /**
+     * Inserts record into database
+     *
+     * @param burnedCalories
+     */
     void create(BurnedCalories burnedCalories);
+
+    /**
+     * Updates record in database
+     *
+     * @param burnedCalories
+     */
     void update(BurnedCalories burnedCalories);
+
+    /**
+     * Deletes record from database
+     *
+     * @param burnedCalories
+     */
     void delete(BurnedCalories burnedCalories);
 }
--- CaloriesRecordingSystem/src/main/java/com/muni/fi/pa165project/entity/Record.java
+++ CaloriesRecordingSystem/src/main/java/com/muni/fi/pa165project/entity/Record.java
@@ -6,28 +6,39 @@
 package com.muni.fi.pa165project.entity;
 
 import com.muni.fi.pa165project.enums.Difficulty;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
 import java.time.LocalDateTime;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 /**
- *
  * @author Peter Krasnan
  */
 @Entity
-public class Record {
+public class Record implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
+    @ManyToOne(optional=false)
+    private Activity activity;
+
+    @NotNull
+    private User user;
+
+    @NotNull
     private LocalDateTime time;
 
-    private int burnedCalories;
-
+    @NotNull
+    @Enumerated(EnumType.STRING)
     private Difficulty difficulty;
+
+    private int weight;
+
+    private int burnedCalories;
 
     private int distance;
 
@@ -79,5 +90,118 @@ public class Record {
 
     public void setDuration(int duration) {
         this.duration = duration;
+    }
+
+    public Activity getActivity() {
+        return activity;
+    }
+
+    public void setActivity(Activity activity) {
+        this.activity = activity;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((user == null) ? 0 : user.hashCode());
+        result = prime * result + ((time == null) ? 0 : time.hashCode());
+        result = prime * result + ((activity == null) ? 0 : activity.hashCode());
+        result = prime * result + ((difficulty == null) ? 0 : difficulty.hashCode());
+        result = prime * result + weight;
+        result = prime * result + burnedCalories;
+        result = prime * result + distance;
+        result = prime * result + duration;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof Record)) {
+            return false;
+        }
+
+        final Record other = (Record) obj;
+
+        if (activity == null) {
+            return false;
+        } else {
+            if (!activity.equals(other.activity)) {
+                return false;
+            }
+        }
+
+        if (user == null) {
+            return false;
+        } else {
+            if (!user.equals(other.user)) {
+                return false;
+            }
+        }
+
+        if (time == null) {
+            return false;
+        } else {
+            if (!time.equals(other.time)) {
+                return false;
+            }
+        }
+
+        if (user == null) {
+            return false;
+        } else {
+            if (!user.equals(other.user)) {
+                return false;
+            }
+        }
+
+        if (difficulty == null) {
+            return false;
+        } else {
+            if (!difficulty.equals(other.difficulty)) {
+                return false;
+            }
+        }
+
+        if (weight != other.weight) {
+            return false;
+        }
+
+        if (distance != other.distance) {
+            return false;
+        }
+
+        if (duration != other.duration) {
+            return false;
+        }
+
+        if (burnedCalories != other.burnedCalories) {
+            return false;
+        }
+
+        return true;
     }
 }
