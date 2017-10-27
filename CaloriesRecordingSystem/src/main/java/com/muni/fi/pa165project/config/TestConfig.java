e5cb9fc64bc324d21f35d86e9459a0b0bbc70b58
Radoslav Karlik (422358) <karlik.radoslav@gmail.com>
2017-10-27 09:59:34 +0200

    Refactor TestConfig, BurnedCaloriesDaoImplTest

--- CaloriesRecordingSystem/src/main/java/com/muni/fi/pa165project/config/TestConfig.java
+++ CaloriesRecordingSystem/src/main/java/com/muni/fi/pa165project/config/TestConfig.java
@@ -5,7 +5,10 @@
  */
 package com.muni.fi.pa165project.config;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -19,11 +22,13 @@ import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
  */
 @Configuration
 @Import(AppConfig.class)
-public class InMemoryDatabaseContext {
+public class TestConfig {
+	
 	@Bean
 	public DataSource dataSource(){
 		EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
 		EmbeddedDatabase db = builder.setType(EmbeddedDatabaseType.DERBY).build();
 		return db;
 	}
+	
 }
--- CaloriesRecordingSystem/src/test/java/com/muni/fi/pa165project/dao/BurnedCaloriesDaoImplTest.java
+++ CaloriesRecordingSystem/src/test/java/com/muni/fi/pa165project/dao/BurnedCaloriesDaoImplTest.java
@@ -5,10 +5,12 @@
  */
 package com.muni.fi.pa165project.dao;
 
-import com.muni.fi.pa165project.config.InMemoryDatabaseContext;
+import com.muni.fi.pa165project.config.TestConfig;
+import com.muni.fi.pa165project.entity.Activity;
 import com.muni.fi.pa165project.entity.BurnedCalories;
 import com.muni.fi.pa165project.enums.Difficulty;
 import java.util.List;
+import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 import org.junit.Assert;
 import org.junit.Before;
@@ -23,86 +25,99 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * @author Radoslav Karlik
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = InMemoryDatabaseContext.class)
+@ContextConfiguration(classes = TestConfig.class)
 @Transactional
 public class BurnedCaloriesDaoImplTest {
 
-       @Autowired
-       private BurnedCaloriesDao bcDao;
+	@Autowired
+	private BurnedCaloriesDao bcDao;
 
-       private BurnedCalories bc1;
-       private BurnedCalories bc2;
-       private BurnedCalories bc3;
+	@Autowired
+	private ActivityDao acDao;
 
-       @Before
-       public void init() {
-               this.bc1 = new BurnedCalories();
-               this.bc1.setWeightRange(100);
+	private BurnedCalories bc1;
+	private BurnedCalories bc2;
+	private BurnedCalories bc3;
 
-               this.bc2 = new BurnedCalories();
-               this.bc2.setWeightRange(200);
+	private Activity ac;
+	
+	@Transactional
+	private void createDummyActivity() {
+		this.ac = new Activity();
+		this.ac.setName("Activity 1");
+		this.ac.setDescription("test activity");
+		this.acDao.create(ac);
+	}
+	
+	@Before
+	public void init() {
+		createDummyActivity();
 
-               this.bc3 = new BurnedCalories();
-               this.bc3.setWeightRange(300);
+		this.bc1 = new BurnedCalories();
+		this.bc1.setWeightRange(100);
+		this.bc1.setActivity(ac);
+		this.bc1.setDifficulty(Difficulty.MEDIUM);
+		
+		this.bc2 = new BurnedCalories();
+		this.bc2.setWeightRange(200);
+		this.bc2.setActivity(ac);
+		this.bc2.setDifficulty(Difficulty.EASY);
+		
+		this.bc3 = new BurnedCalories();
+		this.bc3.setWeightRange(300);
+		this.bc3.setActivity(ac);
+		this.bc3.setDifficulty(Difficulty.HARD);
 
-               this.bcDao.create(bc1);
-               this.bcDao.create(bc2);
-               this.bcDao.create(bc3);
-       }
+		this.bcDao.create(bc1);
+		this.bcDao.create(bc2);
+		this.bcDao.create(bc3);
+	}
 
-       @Test
-       @Rollback(true)
-       public void testCreate() {
-               BurnedCalories bc = new BurnedCalories();
+	@Test
+	@Rollback(true)
+	public void testCreate() {
+		BurnedCalories bc = new BurnedCalories();
 
-               bc.setWeightRange(75);
-               bc.setAmount(200);
-               bc.setDifficulty(Difficulty.MEDIUM);
-               //bc.setActivity(ac);
+		bc.setWeightRange(75);
+		bc.setAmount(200);
+		bc.setDifficulty(Difficulty.MEDIUM);
+		bc.setActivity(ac);
 
-               this.bcDao.create(bc);
+		this.bcDao.create(bc);
 
-               List<BurnedCalories> bcs = this.bcDao.findAll();
-               Assert.assertTrue(bcs.size() == 1);
-       }
+		List<BurnedCalories> bcs = this.bcDao.findAll();
+		Assert.assertTrue(bcs.size() == 4);
+	}
 
-       @Test
-       @Rollback(true)
-       public void testUpdate() {
-               this.bc2.setWeightRange(201);
-               this.bcDao.update(bc2);
+	@Test
+	@Rollback(true)
+	public void testUpdate() {
+		this.bc2.setWeightRange(201);
+		this.bcDao.update(bc2);
 
-               BurnedCalories bc2FromDB = this.bcDao.findById(this.bc2.getId());
+		BurnedCalories bc2FromDB = this.bcDao.findById(this.bc2.getId());
 
-               Assert.assertEquals(201, bc2FromDB.getWeightRange());
-       }
+		Assert.assertEquals(201, bc2FromDB.getWeightRange());
+	}
 
-       @Test
-       @Rollback(true)
-       public void testRemove() {
-               int initialCount = this.bcDao.findAll().size();
+	@Test
+	@Rollback(true)
+	public void testRemove() {
+		int initialCount = this.bcDao.findAll().size();
 
-               this.bcDao.delete(bc1);
+		this.bcDao.delete(bc1);
 
-               int finalCount = this.bcDao.findAll().size();
+		int finalCount = this.bcDao.findAll().size();
 
-               Assert.assertEquals(initialCount, finalCount + 1);
-       }
+		Assert.assertEquals(initialCount, finalCount + 1);
+	}
 
-       @Test
-       @Rollback(true)
-       public void testFindAll() {
-               List<BurnedCalories> bcs = this.bcDao.findAll();
+	@Test
+	@Rollback(true)
+	public void testFindAll() {
+		List<BurnedCalories> bcs = this.bcDao.findAll();
 
-               Assert.assertTrue(bcs.size() == 3);
-       }
-
-       @Test
-       @Rollback(true)
-       public void testFindById() {
-               BurnedCalories fromDb = this.bcDao.findById(1);
-
-               Assert.assertEquals(100, fromDb.getWeightRange());
-       }
+		Assert.assertTrue(bcs.size() == 3);
+	}
 
 }
