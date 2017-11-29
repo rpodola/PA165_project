da22f04ab4da5df34097e4c320fbee04549b6086
ciso112 <lukas.cisar@gmail.com>
2017-11-29 23:33:46 +0100

    UsersController scratch edit

---
+++ CaloriesRecordingSystem/crs-rest/src/main/java/com/muni/fi/pa165project/rest/controllers/UsersController.java
@@ -0,0 +1,38 @@
+package com.muni.fi.pa165project.rest.controllers;
+
+
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * REST Controller for Users
+ * 
+ * @author Lukáš Císar
+ */
+
+@RestController
+//@RequestMapping(ApiUris.ROOT_URI_ORDERS)
+  public class UsersController {
+//	 final static Logger logger = LoggerFactory.getLogger(UserController.class);
+//
+//	    @Inject
+//	    private UserFacade userFacade;
+	    
+//	    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+//	    public final UserDTO getUser(@RequestParam(@PathVariable("user_id") long userId)) {
+//	        
+//	        logger.debug("rest getOrders({},{})", lastWeek, status);
+//
+//	       
+//	        }
+
+}
