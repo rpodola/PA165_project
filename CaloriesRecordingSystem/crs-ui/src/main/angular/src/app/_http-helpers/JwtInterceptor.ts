8a2634d99ac36fd8c05f27d295676c78909dca32
Radoslav Karlik (422358) <karlik.radoslav@gmail.com>
2017-12-16 23:33:30 +0100

    [angular] Add redirect when 401/403 status code returned

--- CaloriesRecordingSystem/crs-ui/src/main/angular/src/app/_http-helpers/JwtInterceptor.ts
+++ CaloriesRecordingSystem/crs-ui/src/main/angular/src/app/_http-helpers/JwtInterceptor.ts
@@ -1,9 +1,16 @@
 import {Injectable} from '@angular/core';
-import {HTTP_INTERCEPTORS, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/do';
+import {Router} from '@angular/router';
 
 @Injectable()
 class JwtInterceptor implements HttpInterceptor {
+  constructor(
+    private router: Router,
+  ) { }
+
+
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const currentUser = JSON.parse(localStorage.getItem('currentUser'));
 
@@ -15,7 +22,18 @@ class JwtInterceptor implements HttpInterceptor {
       });
     }
 
-    return next.handle(request);
+    return next
+      .handle(request)
+      .do(
+        null,
+        (err) => {
+          if (err instanceof HttpErrorResponse) {
+            if (err.status === 401 || err.status === 403) {
+              this.router.navigateByUrl('/login');
+            }
+          }
+        }
+      );
   }
 }
 
